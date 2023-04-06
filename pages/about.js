81fe9df2d5bd13dad2ad08592aa096e95f631df2
kishan-2029 <kishan.prajapati@searce.com>
2023-04-06 19:11:18 +0530

    second commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Meta from '../components/Meta'
+function about() {
+    return (
+        <>
+            <Meta title='About' />
+            <div><h1>about us</h1></div>
+        </>
+    )
+}
+
+export default about
